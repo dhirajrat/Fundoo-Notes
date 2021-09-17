1675000080ae4480c68a788091952a779d2e4030
dhirajrathod <dhirajrathod3000@gmail.com>
2021-09-17 14:47:49 +0530

    Refactor: Req Body Validation Refactored

--- Fundoo_Notes/app/controllers/user.controller.js
+++ Fundoo_Notes/app/controllers/user.controller.js
@@ -1,5 +1,5 @@
 const userService = require("../service/user.service.js");
-const validatorObj = require('../validation/validation.js');
+const validatorObj = require('../userValidation/validation.js');
 
 // Controller Class
 class Controller {
@@ -14,30 +14,36 @@ class Controller {
 
       const validUser = validatorObj.auth.validate(user);
 
-      if(!validUser.error){
-        userService.registerUser(user, (error, data) => {
-          if (error) {
-            return res.status(409).json({
-              success: false,
-              message: "Already exist User",
-            });
-          } else {
-            res.status(201).json({
-              success: true,
-              data: data,
-              message: "User Data Inserted successfully",
-            });
-          }
-        });
+      if (validUser.error) {
+        console.log(validUser.error)
+        return res.status(400).send({
+          success: false,
+          message: 'Invalid Input',
+          data: validUser
+        });           
       }
 
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        data: null,
-        message: "server-error",
+      userService.registerUser(user, (error, data) => {
+        if (error) {
+          return res.status(409).json({
+            success: false,
+            message: "Already exist User",
+          });
+        } else {
+          res.status(201).json({
+            success: true,
+            data: data,
+            message: "User Data Inserted successfully",
+          });
+        }
       });
-    }
+      } catch (error) {
+          return res.status(500).json({
+            success: false,
+            data: null,
+            message: "server-error",
+          });
+        }
   };
 
   
