a1f0669a2ec8277891cb906ba62836977f2d3b05
dhirajrathod <dhirajrathod3000@gmail.com>
2021-10-07 19:26:39 +0530

    refactor: Nodemailer function Refactored

--- Fundoo_Notes/app/service/user.service.js
+++ Fundoo_Notes/app/service/user.service.js
@@ -1,10 +1,8 @@
 const bcrypt = require("bcrypt");
 const userModel = require("../models/user.model.js");
 const helper = require("../utility/helper.js");
-const jwt = require('jsonwebtoken');
 const logger = require('../../logger/logger');
-const nodemailer = require("nodemailer");
-
+const sendLinkMail = require('../utility/sendresetlink')
 class userService {
   /**
    * Register User
@@ -69,32 +67,8 @@ class userService {
         helper.jwtTokenGenerate(data, secretkey, (err, token) =>{
           if(token){
             console.log("service forget id and token : ",data.id," ",token);
-
-            const link = `http://localhost:${process.env.PORT}/resetpassword/${token}`;
-            // create reusable transporter object using the default SMTP transport
-            let transporter = nodemailer.createTransport({
-              service: "gmail",
-              auth: {
-                user: process.env.NODEMAILER_G_USER, // generated ethereal user
-                pass: process.env.NODEMAILER_G_PASS, // generated ethereal password
-              },
-            });
-
-            // send mail with defined transport object
-            let info = transporter.sendMail({
-              from: '"Fundoo Notes" <no-reply@fundoonotes.com>', // sender address
-              to: data.email, // list of receivers
-              subject: "Reset Password - Fundoo notes account", // Subject line
-              text: `Hello ${data.firstName}.`, // plain text body
-              html: `<b>Hello ${data.firstName}. Here is your link to reset Password: <button href="${link}"> <a href="${link}">reset password</a></button></b>`, // html body
-            });
-
-            console.log("Message sent: %s", info.messageId);
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-            // Preview only available when sending through an Ethereal account
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
+            // Send mail
+            sendLinkMail.sendResetPasswordMail(token, data);
             return callback(null, token);
           }
           else {
